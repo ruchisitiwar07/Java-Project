c869544d6770bb5f6151d345d5ff2245aa899425
ruchisitiwar07 <rsitiwar@gmail.com>
2026-03-18 23:12:40 +0530

    Variables practice

---
+++ JAVA-18-03-2026/Variables.java
@@ -0,0 +1,24 @@
+ 
+ public class Variables {
+
+
+ public static void main (String[] args) {
+
+        //declaring variables
+        int a = 10;
+        double b = 5.5;
+        String c = "Hello";
+        boolean e = true;
+        int d;
+        d=20;
+        float f = 3.14f;
+
+        //printing variables
+        System.out.println("Integer: " + a);
+        System.out.println("Double: " + b);
+        System.out.println("String: " + c);
+        System.out.println("Integer: " + d);
+        System.out.println("Boolean: " + e);
+        System.out.println(a+d);
+    }
+}
