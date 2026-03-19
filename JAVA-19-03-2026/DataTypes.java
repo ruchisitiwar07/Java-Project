ae69681d8e07ea7b47e0cdd6acb720f872785e65
ruchisitiwar07 <rsitiwar@gmail.com>
2026-03-19 22:38:15 +0530

    data types

---
+++ JAVA-19-03-2026/DataTypes.java
@@ -0,0 +1,27 @@
+
+
+public class DataTypes {
+
+        public static void main (String[] args) {
+
+                //declaring variables
+                int a = 10;
+                double b = 5.5;
+                String c = "Hello";
+                boolean e = true;
+                int d;
+                d=20;
+                long g = 1226545l;
+                float f = 3.14f;
+
+                //printing variables
+                System.out.println("Integer: " + a);
+                System.out.println("Double: " + b);
+                System.out.println("String: " + c);
+                System.out.println("Integer: " + d);
+                System.out.println("Boolean: " + e);
+                System.out.println(a+d);
+                System.out.println("Long: " + g);
+        }
+    
+}
