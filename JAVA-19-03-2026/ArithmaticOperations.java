c017ad386a947f691a17821a6a16c630545a2699
ruchisitiwar07 <rsitiwar@gmail.com>
2026-03-22 18:55:35 +0530

    exercise2

---
+++ JAVA-19-03-2026/ArithmaticOperations.java
@@ -0,0 +1,12 @@
+public class ArithmaticOperations {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        a = a +5;
+        System.out.println(a);
+
+        int b = 20;
+        b += 10;
+        System.out.println(b);
+    }
+}
---
+++ JAVA-19-03-2026/Exercise2.java
@@ -0,0 +1,31 @@
+public class Exercise2 {
+    public static void main(String[] args){
+     
+         // write code here
+         
+            int a = 20;
+            int b = 10;
+            
+            int Addition = a + b;
+            System.out.println("Addition: " +  Addition);
+            
+            int Subtraction = a-b;
+            System.out.println("Subtraction: " +  Subtraction);
+            
+            int Multiplication = a * b;
+            System.out.println("Multiplication: " +  Multiplication);
+            
+            int Division = a / b;
+            System.out.println("Division: " +  Division);
+            
+            boolean result = a > b;
+            System.out.println("Is a greater than b? " + result);
+            
+            result = a > b && b > 0;
+            System.out.println("Is a > b and b > 0? " + result);
+            
+         
+    }
+}
+    
+
