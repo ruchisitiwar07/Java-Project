9988299861b7bf00c8a64fdb43bdd36ce242f58b
ruchisitiwar07 <rsitiwar@gmail.com>
2026-03-23 22:46:31 +0530

    ifelse

---
+++ JAVA_23_03_2026/IfElse.java
@@ -0,0 +1,17 @@
+
+
+public class IfElse {
+    public static void main(String[] args) {
+        
+        int a = 4;
+        int b = 6;
+
+        if(a>b)
+
+            System.out.println("a is greater than b");
+
+        else
+
+            System.out.println("b is greater than a");
+    }
+}
